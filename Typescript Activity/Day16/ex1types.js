8460713086973346e714b24cbf106183c96513df
sumitsingh7773 <89596003+sumitsingh7773@users.noreply.github.com>
2022-03-15 19:02:23 +0530

    Day15 TypeScript Activity

---
+++ Typescript Activity/Day16/ex1types.js
@@ -0,0 +1,6 @@
+var username = "Alexandar";
+var age = 35;
+var isEmployed = true;
+var contacts = { phone: 99999, email: "alex@gmail.com" };
+document.write("<p>Username:".concat(username, ", Age:").concat(age, ", isEmployed: ").concat(isEmployed, "</p>"));
+document.write("<p>Phone: ".concat(contacts.phone, ", Email: ").concat(contacts.email, "</p>"));
---
+++ Typescript Activity/Day16/ex1types.ts
@@ -0,0 +1,7 @@
+let username : string = "Alexandar";
+let age : number = 35;
+let isEmployed : boolean = true;
+let contacts : any = {phone: 99999, email: "alex@gmail.com"};
+
+document.write(`<p>Username:${username}, Age:${age}, isEmployed: ${isEmployed}</p>`);
+document.write(`<p>Phone: ${contacts.phone}, Email: ${contacts.email}</p>`);
