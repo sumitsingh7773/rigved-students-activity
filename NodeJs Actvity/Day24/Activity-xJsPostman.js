65f022c21cdd822438d154e7204cb487dadf61bc
sumitsingh7773 <sumitsingh7773@gmail.com>
2022-03-25 20:01:44 +0530

    modify

--- NodeJs Actvity/Day24/Activity-xJsPostman.js
+++ NodeJs Actvity/Day24/Activity-xJsPostman.js
@@ -6,7 +6,7 @@ console.log(employeeArray);
 let express = require("express");
 let app = express();
 app.listen(3004,()=>{
-console.log('Server is running on Port 3004');
+console.log(`Server is running on Port 3004`);
 });
 app.get('/',(request,response)=>{
     response.send("Hello Sumit! ");
